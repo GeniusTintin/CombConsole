d7359db5d2f20f3974c681dee00ed83c429b7235
Dingran Yuan <tintinpost@outlook.com>
2021-04-11 18:28:08 +1000

    Add project files.

---
+++ CombFilter.cpp
@@ -0,0 +1,13 @@
+#include "CombFilter.h"
+#include <iostream>
+
+namespace CombFilter {
+
+	combFilter::combFilter() {
+
+	}
+
+	combFilter::~combFilter() {
+
+	}
+}
---
+++ CombFilter.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/core.hpp>
+
+namespace CombFilter {
+	class combFilter {
+	public:
+		combFilter();
+		~combFilter();
+
+	private:
+	};
+}
